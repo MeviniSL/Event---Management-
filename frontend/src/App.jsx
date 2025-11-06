7ce22055a4953680c3b2a29b68af22e59e5f5d35
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2025-11-07 02:01:40 +0530

    Add project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       Event Management System
+      </div>
+    </>
+  )
+}
+
+export default App
