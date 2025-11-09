3b237f5ab57e40030ba5969b0c8a0647560e583f
NavinduR2001 <164781229+NavinduR2001@users.noreply.github.com>
2025-11-09 21:47:05 +0530

    Add files

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div class="w-full h-full m-0 p-0">
+      <Navbar/>
+
+      <div className="flex justify-center items-center home-container h-150">
+      
+      Content
+
+      </div>
+
+      <Footer/>
+    </div>
   )
 }
 
