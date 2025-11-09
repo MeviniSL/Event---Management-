ef864180884f68fb1f35d2b33a6cad29ca8a55e5
Mevini Silva <silvamevini19@gmail.com>
2025-11-09 21:52:24 +0530

    add register feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
 
-import './App.css'
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <div>
-       Event Management System
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" replace />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
